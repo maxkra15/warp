08fb4cafc84c4a294fac732f225f44d3b19c1ff4
Gilles Daviet <gdaviet@nvidia.com>
2026-06-15 08:55:10 +0200

    Replace existing Points-to-grid builder

--- warp/_src/context.py
+++ warp/_src/context.py
@@ -6319,6 +6319,11 @@ class Runtime:
                 ctypes.c_void_p,
                 ctypes.c_uint32,
                 ctypes.c_char_p,
+                ctypes.c_bool,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_void_p,
             ]
             self.core.wp_volume_from_tiles_device.restype = ctypes.c_uint64
             self.core.wp_volume_index_from_tiles_device.argtypes = [
@@ -6328,6 +6333,11 @@ class Runtime:
                 ctypes.c_float * 9,
                 ctypes.c_float * 3,
                 ctypes.c_bool,
+                ctypes.c_bool,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_void_p,
             ]
             self.core.wp_volume_index_from_tiles_device.restype = ctypes.c_uint64
             self.core.wp_volume_from_active_voxels_device.argtypes = [
@@ -6337,51 +6347,14 @@ class Runtime:
                 ctypes.c_float * 9,
                 ctypes.c_float * 3,
                 ctypes.c_bool,
+                ctypes.c_bool,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_uint32,
+                ctypes.c_void_p,
             ]
             self.core.wp_volume_from_active_voxels_device.restype = ctypes.c_uint64
-            self.core.wp_volume_from_tiles_device_rebuildable.argtypes = [
-                ctypes.c_void_p,
-                ctypes.c_void_p,
-                ctypes.c_int,
-                ctypes.c_float * 9,
-                ctypes.c_float * 3,
-                ctypes.c_bool,
-                ctypes.c_void_p,
-                ctypes.c_uint32,
-                ctypes.c_char_p,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_void_p,
-            ]
-            self.core.wp_volume_from_tiles_device_rebuildable.restype = ctypes.c_uint64
-            self.core.wp_volume_index_from_tiles_device_rebuildable.argtypes = [
-                ctypes.c_void_p,
-                ctypes.c_void_p,
-                ctypes.c_int,
-                ctypes.c_float * 9,
-                ctypes.c_float * 3,
-                ctypes.c_bool,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_void_p,
-            ]
-            self.core.wp_volume_index_from_tiles_device_rebuildable.restype = ctypes.c_uint64
-            self.core.wp_volume_from_active_voxels_device_rebuildable.argtypes = [
-                ctypes.c_void_p,
-                ctypes.c_void_p,
-                ctypes.c_int,
-                ctypes.c_float * 9,
-                ctypes.c_float * 3,
-                ctypes.c_bool,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_uint32,
-                ctypes.c_void_p,
-            ]
-            self.core.wp_volume_from_active_voxels_device_rebuildable.restype = ctypes.c_uint64
             self.core.wp_volume_rebuild_from_tiles_device.argtypes = [
                 ctypes.c_uint64,
                 ctypes.c_void_p,
